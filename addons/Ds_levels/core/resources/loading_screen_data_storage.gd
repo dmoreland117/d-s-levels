32a86aa715793f51b59aebd90602e8d585af0521
dmoreland117 <61122492+dmoreland117@users.noreply.github.com>
2025-07-16 12:28:28 -0400

    added LoadingScreenDataStorage

---
+++ addons/Ds_levels/core/resources/loading_screen_data_storage.gd
@@ -0,0 +1,104 @@
+@tool
+class_name LoadingScreenDataStorage
+extends Resource
+
+
+signal data_updated()
+
+@export var loading_screen_datas:Array[Dictionary] = []:
+	set(val):
+		loading_screen_datas = val
+
+		data_updated.emit()
+
+
+func add_data(label:String, path:String) -> bool:
+	if !loading_screen_datas:
+		return false
+	
+	loading_screen_datas.append(
+		{
+			'label':label,
+			'path': path
+		}
+	)
+	
+	data_updated.emit()
+	return true
+	
+func edit_data(index:int, data:Dictionary) -> bool:
+	if !loading_screen_datas:
+		return false
+	
+	if !index <= loading_screen_datas.size():
+		return false
+	
+	loading_screen_datas[index] = data
+	
+	data_updated.emit()
+	return true
+	
+func remove_data(index:int) -> bool:
+	if !loading_screen_datas:
+		return false
+	
+	if !index <= loading_screen_datas.size():
+		return false
+	
+	loading_screen_datas.remove_at(index)
+	
+	data_updated.emit()
+	return true
+	
+func get_data_list() -> Array[Dictionary]:
+	if !loading_screen_datas:
+		return []
+	
+	return loading_screen_datas
+	
+func get_data_by_label(label:String) -> Dictionary:
+	if !loading_screen_datas:
+		return {}
+	
+	for screen in loading_screen_datas:
+		if screen.get('label') == label:
+			return screen
+	
+	return {}
+	
+func get_data_by_index(index:int) -> Dictionary:
+	if !loading_screen_datas:
+		return {}
+	
+	if !index <= loading_screen_datas.size():
+		return {}
+	
+	return loading_screen_datas[index]
+	
+func has_data(label:String) -> bool:
+	if !loading_screen_datas:
+		return false
+	
+	for screen in loading_screen_datas:
+		if screen.get('label') == label:
+			return true
+	
+	return false
+	
+func save_at_settings_path() -> bool:
+	var storage_path = LevelManagerPlugin.get_loading_screen_storage_path()
+	if !storage_path.is_valid_filename():
+		return false
+
+	ResourceSaver.save(self, storage_path)
+	return true
+	
+static func load_from_settings_path() -> LoadingScreenDataStorage:
+	var storage_path = LevelManagerPlugin.get_loading_screen_storage_path()
+
+	if !ResourceLoader.exists(storage_path):
+		return
+	
+	var new_storage = load(storage_path)
+
+	return new_storage
