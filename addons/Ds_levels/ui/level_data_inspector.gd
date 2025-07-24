2b7a5e76385f6953ce1b85c814e191485af6475f
dmoreland117 <61122492+dmoreland117@users.noreply.github.com>
2025-07-24 12:29:20 -0400

    Closes #17

--- addons/Ds_levels/ui/level_data_inspector.gd
+++ addons/Ds_levels/ui/level_data_inspector.gd
@@ -13,6 +13,7 @@ signal saved()
 @onready var loading_screen_background_picker: HBoxContainer = %loading_screen_background_picker
 @onready var loading_screen_label_dropdown: OptionButton = %loading_screen_label_dropdown
 @onready var level_hidden_input: CheckBox = %level_hidden_input
+@onready var set_default_button: Button = %set_default_button
 
 var index:int = -1
 var data:LevelData
@@ -26,38 +27,20 @@ func set_index(idx:int):
 	
 	if index == -1 or !LevelDataStorage.is_storage_loaded():
 		data = null
-	
-		level_label_input.text = ''
-		level_description_input.text = ''
-		level_path_picker.current_path = ''
-		loading_screen_background_picker.current_path = ''
-		loading_screen_label_dropdown.clear()
-		margin_container.hide()
+		clear_inspector()
 		
 		return
 	
 	data = LevelDataStorage.get_data_by_index(index)
-	
-	level_label_input.text = data.label
-	level_description_input.text = data.description
-	level_path_picker.start_path = data.level_path
-	level_hidden_input.button_pressed = data.hidden
-	show_level_loading_screen_input.button_pressed = data.show_loading_screen
-	loading_screen_background_picker.start_path = data.loading_screen_background_path
-	
-	loading_screen_label_dropdown.clear()
-	if LoadingScreenDataStorage.is_storage_loaded():
-		var loading_screen_datas = LoadingScreenDataStorage.get_data_list()
-		for screen in loading_screen_datas:
-			loading_screen_label_dropdown.add_item(screen.get('label', '[Error]'))
-		
-		loading_screen_label_dropdown.select(
-			LoadingScreenDataStorage.get_index_by_label(data.loading_screen_name)
-		)
-	
-	loading_screen_background_picker.current_path = data.loading_screen_background_path
+	_populate_inspector()
 	
 	margin_container.show()
+	
+	if !data or !LevelDataStorage.is_start_level(data):
+		set_default_button.disabled = false
+		return
+	
+	set_default_button.disabled = true
 
 func _on_save_button_pressed() -> void:
 	if !LevelDataStorage.is_storage_loaded():
@@ -77,10 +60,10 @@ func _on_save_button_pressed() -> void:
 	saved.emit()
 
 func _on_reset_button_pressed() -> void:
-	pass # Replace with function body.
+	set_index(index)
 
 func _on_clear_button_pressed() -> void:
-	pass # Replace with function body.
+	clear_inspector()
 
 func _on_set_default_button_pressed() -> void:
 	if !LevelDataStorage.is_storage_loaded():
@@ -88,3 +71,35 @@ func _on_set_default_button_pressed() -> void:
 		return
 	
 	LevelDataStorage.set_start_level(index)
+
+func _populate_inspector():
+	if !data:
+		return
+	
+	level_label_input.text = data.label
+	level_description_input.text = data.description
+	level_path_picker.start_path = data.level_path
+	level_hidden_input.button_pressed = data.hidden
+	show_level_loading_screen_input.button_pressed = data.show_loading_screen
+	loading_screen_background_picker.start_path = data.loading_screen_background_path
+	
+	loading_screen_label_dropdown.clear()
+	if LoadingScreenDataStorage.is_storage_loaded():
+		var loading_screen_datas = LoadingScreenDataStorage.get_data_list()
+		for screen in loading_screen_datas:
+			loading_screen_label_dropdown.add_item(screen.get('label', '[Error]'))
+		
+		loading_screen_label_dropdown.select(
+			LoadingScreenDataStorage.get_index_by_label(data.loading_screen_name)
+		)
+	
+	loading_screen_background_picker.current_path = data.loading_screen_background_path
+
+func clear_inspector():
+	level_label_input.text = ''
+	level_description_input.text = ''
+	level_path_picker.start_path = ''
+	level_hidden_input.button_pressed = false
+	show_level_loading_screen_input.button_pressed = false
+	loading_screen_background_picker.start_path = ''
+	loading_screen_label_dropdown.select(0)
