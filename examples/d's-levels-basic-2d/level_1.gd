81eb04d50759b2bfeee49011c76b4bca9fd47a3f
dmoreland117 <61122492+dmoreland117@users.noreply.github.com>
2025-08-02 12:27:27 -0400

    Updated examples

--- examples/d's-levels-basic-2d/level_1.gd
+++ examples/d's-levels-basic-2d/level_1.gd
@@ -2,6 +2,6 @@
 extends Level2D
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		Levels.change_to_level_name('level 2', 'spawn')
