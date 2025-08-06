3d9647ef163c9934ab5b6f5362634a7be622d407
dmoreland117 <61122492+dmoreland117@users.noreply.github.com>
2025-08-06 14:39:21 -0400

    Add: More default Transitions

---
+++ addons/Ds_levels/level_transitions/wipe_right_transition.gd
@@ -0,0 +1,16 @@
+extends LevelTransition
+
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _transition_in():
+	animation_player.play("anim_in")
+
+func _transition_out():
+	animation_player.play("anim_out")
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	
+	transition_finished()
